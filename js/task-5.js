df99ce7ffb64279274d3ff2e1db4a2202dab01d0
Vladyslav84 <72793544+Vladyslav84@users.noreply.github.com>
2021-04-07 13:09:20 +0300

    Виправив 5 і 6 завдання

--- js/task-5.js
+++ js/task-5.js
@@ -3,8 +3,14 @@ const inputEl = document.querySelector('#name-input');
 const greetingEl = document.querySelector('#name-output');
 
 inputEl.addEventListener('input', (e) => {
-    e.preventDefault;
+    e.preventDefault();
+    if (greetingEl.textContent = inputEl.value)
+    {
+        greetingEl.textContent = inputEl.value;
+    } else
+    {
+        greetingEl.textContent = 'незнакомец';
 
-    greetingEl.textContent = inputEl.value;
+    }
 
 });
--- js/task-6.js
+++ js/task-6.js
@@ -3,7 +3,7 @@ const inputEl = document.querySelector('#validation-input');
 inputEl.addEventListener('blur', (e) => {
     e.preventDefault;
 
-    if (inputEl.value.length > inputEl.dataset.length)
+    if (inputEl.value.length >= inputEl.dataset.length)
     {
         inputEl.classList.remove('invalid');
         inputEl.classList.add('valid');
