b55b2ff074809874b7e10dfe5ab5f00051093ec6
Vladyslav84 <72793544+Vladyslav84@users.noreply.github.com>
2021-04-07 16:28:48 +0300

    Виправив 5 і 6 завдання.
    Добавив порівняння.

--- js/task-5.js
+++ js/task-5.js
@@ -2,14 +2,17 @@ const inputEl = document.querySelector('#name-input');
 
 const greetingEl = document.querySelector('#name-output');
 
+let greetingText = greetingEl.textContent;
+
 inputEl.addEventListener('input', (e) => {
     e.preventDefault();
+
     if (greetingEl.textContent = inputEl.value)
     {
         greetingEl.textContent = inputEl.value;
     } else
     {
-        greetingEl.textContent = 'незнакомец';
+        greetingEl.textContent = greetingText;
 
     }
 
--- js/task-6.js
+++ js/task-6.js
@@ -3,15 +3,15 @@ const inputEl = document.querySelector('#validation-input');
 inputEl.addEventListener('blur', (e) => {
     e.preventDefault;
 
-    if (inputEl.value.length >= inputEl.dataset.length)
+    if (inputEl.value.length < inputEl.dataset.length || inputEl.value.length > inputEl.dataset.length)
     {
-        inputEl.classList.remove('invalid');
-        inputEl.classList.add('valid');
+
+        inputEl.classList.add('invalid');
 
     } else
     {
-        inputEl.classList.add('invalid');
+        inputEl.classList.remove('invalid');
+        inputEl.classList.add('valid');
     }
 
 });
-
